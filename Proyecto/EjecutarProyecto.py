ffd7235617bc8303d98997cab40c856ffe9df93e
CarlosCERS <101842946+CarlosCERS@users.noreply.github.com>
2023-10-30 17:29:07 -0600

    Commit RSA_OAEP y archivo
    añadida una función para escribir los resultados en un txt

--- Proyecto/EjecutarProyecto.py
+++ Proyecto/EjecutarProyecto.py
@@ -1,5 +1,6 @@
 import json
 from Algoritmos import chacha20
+from Algoritmos import rsa_oaep
 from Herramientas import Graficar
 
 with open("Proyecto/Claves.json","r") as f:
@@ -7,10 +8,10 @@ with open("Proyecto/Claves.json","r") as f:
 
 Key = my_dict["Key"]
 Ciclos = int(my_dict["Ciclos"])
+Resultado = open('Proyecto/Resultados.txt','w')
 
 ################# Cifrado y Descifrado ######################
 Vectores = ["Proyecto/Vectores/Texto.txt","Proyecto/Vectores/Requerimiento.pdf","Proyecto/Vectores/Imagen.jpeg"]
-
 for archivo in Vectores:
     titulos=[]
     tiemposEncriptacion=[]
@@ -35,17 +36,29 @@ for archivo in Vectores:
     tiemposEncriptacion.append(0)
     tiemposDesencriptacion.append(0)
 
-    # RSA_OAEP
-    if(archivo=="Proyecto/Vectores/Texto.txt"):
-        titulos.append('RSA_OAEP')
-        # Llamada de la función.
-        tiemposEncriptacion.append(0)
-        tiemposDesencriptacion.append(0)
-    
-    # Imprimimos los resultados.
-    tiemposEncriptacion=[1,2,3,4] #Comentar cuando quieren probar los resultados.
-    tiemposDesencriptacion=[5,6,7,8] #Comentar cuando quieren probar los resultados.
+    Resultado.write(f'Encriptacion y desencriptacion: {archivo}, {Ciclos} ciclos\n')
+    for i in range(len(titulos)):
+        Resultado.write(f'{titulos[i]} \n\t Encriptacion:{tiemposEncriptacion[i]} \n\t Desencriptacion:{tiemposDesencriptacion[i]}\n')
+    Resultado.write('\n')
+    # Graficar.
     Graficar.GraficarEncriptadoDesencriptado(archivo,titulos,tiemposEncriptacion,tiemposDesencriptacion,Ciclos)
+
+############# Encriptación y Desencriptación con RSA_OAEP (Solo se aplica al Texto.txt por el límite de tamaño y se encuentra solo por lo lento que es.)
+print(f"Procesando encriptación y desencriptación de Proyecto/Vectores/Texto.txt con RSA_OAEP")
+titulos=[]
+tiemposEncriptacion=[]
+tiemposDesencriptacion=[]
+titulos.append('RSA_OAEP')
+Encriptado, Desencriptado =  rsa_oaep.calcularTiempo("Proyecto/Vectores/Texto.txt",Ciclos)
+tiemposEncriptacion.append(Encriptado)
+tiemposDesencriptacion.append(Desencriptado)
+
+Resultado.write(f'Encriptacion y desencriptacion: Proyecto/Vectores/Texto.txt, {Ciclos} ciclos, con RSA_OAEP\n')
+for i in range(len(titulos)):
+    Resultado.write(f'{titulos[i]} \n\t Encriptacion:{tiemposEncriptacion[i]} \n\t Desencriptacion:{tiemposDesencriptacion[i]}\n')
+Resultado.write('\n')
+# Graficar.
+Graficar.GraficarEncriptadoDesencriptado(archivo,titulos,tiemposEncriptacion,tiemposDesencriptacion,Ciclos)
 
 ################# Hashing ######################
 titulos=['SHA-2','SHA-3','Scrypt']
@@ -54,18 +67,24 @@ for archivo in Vectores:
     print(f"Procesando Hashing de {archivo}")
     # SHA-2
     # Llamada de la función.
-    tiemposHashing.append(0)
+    tiemposHashing.append(1)
 
     # SHA 3
     # Llamada de la función.
-    tiemposHashing.append(0)
+    tiemposHashing.append(2)
 
     # Scrypt
     # Llamada a la función
-    tiemposHashing.append(0)
+    tiemposHashing.append(3)
 
+# Escribir resultados en archivo.
+Resultado.write(f'Hashing: {Ciclos} ciclos\n')
+for i in range(len(titulos)):
+    Resultado.write(f'{titulos[i]} \n\t Tiempo:{tiemposHashing[i]} archivo .txt\n')
+    Resultado.write(f'\t Tiempo:{tiemposHashing[i+(1*len(titulos))]} archivo .pdf\n')
+    Resultado.write(f'\t Tiempo:{tiemposHashing[i+(2*len(titulos))]} archivo .jpeg\n')
+Resultado.write('\n')
 # Imprimir resultados
-tiemposHashing=[1,2,3,4,5,6,7,8,9] #Comentar cuando quieren probar los resultados.
 Graficar.GraficarComparacionUnSentido(titulos,tiemposHashing, 'Hashing')
     
 ################# Signing ######################
@@ -76,18 +95,24 @@ for archivo in Vectores:
 
     # RSA-PSS
     # Llamada de la función.
-    tiemposFirma.append(0)
+    tiemposFirma.append(4)
 
     # ECDSA P521
     # Llamada de la función.
-    tiemposFirma.append(0)
+    tiemposFirma.append(5)
 
     # ED25519
     # Llamada de la función.
-    tiemposFirma.append(0)
+    tiemposFirma.append(6)
 
+
+Resultado.write(f'Signing: {Ciclos} ciclos\n')
+for i in range(len(titulos)):
+    Resultado.write(f'{titulos[i]} \n\t Tiempo:{tiemposFirma[i]} archivo .txt\n')
+    Resultado.write(f'\t Tiempo:{tiemposFirma[i+(1*len(titulos))]} archivo .pdf\n')
+    Resultado.write(f'\t Tiempo:{tiemposFirma[i+(2*len(titulos))]} archivo .jpeg\n')
+Resultado.write('\n')
 # Imprimir resultados
-tiemposFirma=[1,2,3,4,5,6,7,8,9] #Comentar cuando quieren probar los resultados.
 Graficar.GraficarComparacionUnSentido(titulos,tiemposFirma, 'Signing')
 
 ################# Encriptacion contraseña ######################
@@ -97,21 +122,26 @@ print(f"Procesando Hashing para {Key}")
 
 # SHA-2
 # Llamada a la función
-tiemposContrasena.append(0)
+tiemposContrasena.append(1)
 titulos.append('SHA-2')
 
 # SHA-3
 # Llamada a la función
-tiemposContrasena.append(0)
+tiemposContrasena.append(2)
 titulos.append('SHA-3')
 
 # Scrypt
 # Llamada a la función
-tiemposContrasena.append(0)
+tiemposContrasena.append(3)
 titulos.append('Scrypt')
 
 # Imprimir resultados
-tiemposContrasena=[2,4,6] #Comentar cuando quieren probar los resultados.
+Resultado.write(f'Hashing contrasena: {Key}\n')
+for i in range(len(titulos)):
+    Resultado.write(f'{titulos[i]} \n\t Tiempo:{tiemposContrasena[i]}\n')
+Resultado.write('\n')
 Graficar.GraficarComparacionContrasena(titulos,tiemposContrasena,Key)
 
+# Finalización.
+Resultado.close()
 print('Fin del programa.')
--- Proyecto/Herramientas/Graficar.py
+++ Proyecto/Herramientas/Graficar.py
@@ -13,25 +13,43 @@ def GraficarEncriptadoDesencriptado(archivo, titulos, tiemposCifrado, tiemposDes
     # Obtener el nombre del archivo.
     file_name = os.path.basename(archivo)
 
-    # Declaramos el número de barras a crear.
-    fig, ax = plt.subplots(1, tamanio, figsize=(25, 5), sharey=True)
+    if(tamanio>1):
+        # Declaramos el número de barras a crear.
+        fig, ax = plt.subplots(1, tamanio, figsize=(25, 5), sharey=True)
 
-    for i in range(tamanio):
+        for i in range(tamanio):
+            # Graficar tiempos de encriptación
+            ax[i].bar(0, tiemposCifrado[i], color=colors[i])
+
+            # Graficar tiempos de desencriptación
+            ax[i].bar(1, tiemposDescifrado[i], color=colors[i])
+
+            ax[i].set_xticks([0, 1])
+            ax[i].set_xticklabels(["Cifrado", "Descifrado"])
+            ax[i].set_title(f'{titulos[i]}')
+
+        ax[0].set_ylabel("Tiempo promedio (s)")
+        # Título
+        fig.suptitle(f'Gráficas de tiempo de ejecución de algorítmos de encriptación y desencriptación: {ciclos} ciclos para: {file_name}', fontsize=16)
+        plt.show()
+    else:
+        # Declaramos el número de barras a crear.
+        fig, ax = plt.subplots(1, tamanio, figsize=(15, 5), sharey=True)
+
         # Graficar tiempos de encriptación
-        ax[i].bar(0, tiemposCifrado[i], color=colors[i])
+        ax.bar(0, tiemposCifrado[0], color=colors[0])
 
         # Graficar tiempos de desencriptación
-        ax[i].bar(1, tiemposDescifrado[i], color=colors[i])
+        ax.bar(1, tiemposDescifrado[0], color=colors[0])
 
-        ax[i].set_xticks([0, 1])
-        ax[i].set_xticklabels(["Cifrado", "Descifrado"])
-        ax[i].set_title(f'{titulos[i]}')
+        ax.set_xticks([0, 1])
+        ax.set_xticklabels(["Cifrado", "Descifrado"])
+        ax.set_title(f'{titulos[0]}')
 
-    ax[0].set_ylabel("Tiempo promedio (s)")
-
-    # Título
-    fig.suptitle(f'Gráficas de tiempo de ejecución de algorítmos de encriptación y desencriptación: {ciclos} ciclos para: {file_name}', fontsize=16)
-    plt.show()
+        ax.set_ylabel("Tiempo promedio (s)")
+        # Título
+        fig.suptitle(f'Gráficas de tiempo de ejecución de algorítmos de encriptación y desencriptación: {ciclos} ciclos para: {file_name}', fontsize=16)
+        plt.show()
 
 # Grafica las comparaciones de un solo sentido para distintos archivos.
 def GraficarComparacionUnSentido(titulos, tiempos, accion):
@@ -46,13 +64,13 @@ def GraficarComparacionUnSentido(titulos, tiempos, accion):
 
     for i in range(tamanio):
         # Graficar tiempos de txt
-        ax[i].bar(0, tiempos[i*tamanio], color=colores[0])
+        ax[i].bar(0, tiempos[i], color=colores[0])
 
         # Graficar tiempos de txt
-        ax[i].bar(1, tiempos[i*tamanio+1], color=colores[1])
+        ax[i].bar(1, tiempos[i+(1*tamanio)], color=colores[1])
 
         # Graficar tiempos de txt
-        ax[i].bar(2, tiempos[i*tamanio+2], color=colores[2])
+        ax[i].bar(2, tiempos[i+(2*tamanio)], color=colores[2])
 
         ax[i].set_xticks([0, 1, 2])
         ax[i].set_xticklabels([".txt", ".pdf",".jpeg"])
